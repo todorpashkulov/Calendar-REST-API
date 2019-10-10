71aa5b2830e5977a13a0bc1720159486cbbe063e
todor.pashkulov <blueshadows98@gmail.com>
2019-10-10 16:30:57 +0300

    Initial commit

---
+++ src/main/java/com/musala/calendar/CalendarAppApplication.java
@@ -0,0 +1,13 @@
+package com.musala.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musala/calendar/calendarapp/CalendarAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.musala.calendar.calendarapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
