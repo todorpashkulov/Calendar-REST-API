020ef4f3246c34c6598e2d368e27e363fded46d5
Todor C. Pashkulov <todor.pashkulov@musala.com>
2019-10-15 14:36:31 +0300

    -Initial commit -Added Springfox-swagger dependency

--- src/main/java/com/musala/calendar/CalendarApplication.java
+++ src/main/java/com/musala/calendar/CalendarApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalendarAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CalendarAppApplication.class, args);
-	}
+public class CalendarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarApplication.class, args);
+    }
 
 }
