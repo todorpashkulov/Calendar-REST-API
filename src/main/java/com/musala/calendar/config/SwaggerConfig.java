17e87035b2eb8b33379ad9e70794427c2f286d1e
todor.pashkulov <blueshadows98@gmail.com>
2019-10-15 13:50:52 +0300

    #3 Fixing issues from merge request -fixed import -removed empty space in HelloWorldController.java

--- src/main/java/com/musala/calendar/config/SwaggerConfig.java
+++ src/main/java/com/musala/calendar/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.musala.calendar.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -9,8 +11,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
--- src/main/java/com/musala/calendar/controllers/HelloWorldController.java
+++ src/main/java/com/musala/calendar/controllers/HelloWorldController.java
@@ -1,8 +1,10 @@
 package com.musala.calendar.controllers;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello/")
@@ -13,7 +15,6 @@ public class HelloWorldController {
     public String hello() {
         return "Hello";
     }
-
 
     @PostMapping(value = "post")
     public String postHello() {
